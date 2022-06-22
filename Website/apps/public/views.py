b6bbee2c486c69e42d46da8eff965c74335d4bc3
Carlos Dye <cdye@nevada.unr.edu>
2022-06-22 12:26:12 -0700

    Loose coupling

---
+++ Website/apps/public/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'index.html')
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, 'about.html')
+
+def contact(request: HttpRequest) -> HttpResponse:
+    return render(request, 'contact.html')
--- Website/settings.py
+++ Website/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_URL = "login"
-LOGIN_REDIRECT_URL = "index"
-LOGOUT_REDIRECT_URL = "index"
+LOGIN_REDIRECT_URL = "public:index"
+LOGOUT_REDIRECT_URL = "public:index"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
