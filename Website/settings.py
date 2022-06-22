7e80b7ab6332342401b0d5f19f2970536e19dd50
Carlos Dye <cdye@nevada.unr.edu>
2022-06-21 20:18:57 -0700

    Skeleton+

--- Website/settings.py
+++ Website/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_DIR = os.path.join(BASE_DIR, "Website")
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_DIR, "HTML"), os.path.join(PROJECT_DIR, "Python")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
